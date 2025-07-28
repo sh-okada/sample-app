e97fabf4b2a5b79cd3758c1a338ffcb0477cd152
sh-okada <sh-okada@enecom.co.jp>
2025-07-28 17:13:46 +0900

    リファクタリング

--- api/app/shared/env.py
+++ api/app/shared/env.py
@@ -10,6 +10,6 @@ class DB:
 
 
 class JWT:
-    SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "")
+    SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
     ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
     EXPIRE_MINUTES: int = int(os.getenv("JWT_EXPIRE_MINUTES", 30))
