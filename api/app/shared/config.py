50538a7335ea7812c2dcba8851deaccd1756481b
Shuta-Okada <sh-okada@enecom.co.jp>
2025-08-15 04:05:02 +0000

    リファクタリング

--- api/app/shared/config.py
+++ api/app/shared/config.py
@@ -10,6 +10,8 @@ class DB(BaseSettings):
 
 
 class JWT(BaseSettings):
-    jwt_secret_key: str = "fake_secret_key"
+    jwt_secret_key: str = (
+        "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    )
     jwt_algorithm: str = "HS256"
     jwt_expire_minutes: int = 30
