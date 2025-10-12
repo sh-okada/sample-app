649a20821ddc9bc0fa1a83ee3296246de5f0f247
sh-okada <sh-okada@ymail.ne.jp>
2025-10-12 22:57:38 +0900

    リファクタリング

--- api/tests/test_login.py
+++ api/tests/test_login.py
@@ -12,8 +12,39 @@ from app.main import app
 client = TestClient(app)
 
 
-@pytest.fixture(autouse=True)
-def before_each():
+@pytest.mark.parametrize(
+    "username, password, status_code, json_response",
+    [
+        pytest.param(
+            "sh-okada",
+            "Password123",
+            200,
+            {
+                "id": "5e3868cd-3ec0-4f86-9a94-84363c64da29",
+                "username": "sh-okada",
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTM4NjhjZC0zZWMwLTRmODYtOWE5NC04NDM2M2M2NGRhMjkiLCJleHAiOjE3NTMyMzA2MDB9.cTPZJnsl44nXmY9dfDS7mOc1s11mjCenmdy14FH_wzg",
+            },
+            id="ユーザー名とパスワードでログインできること",
+        ),
+        pytest.param(
+            "bad-user",
+            "Password123",
+            400,
+            {"detail": "Incorrect username or password."},
+            id="ユーザー名が間違っている場合、ログインできないこと",
+        ),
+        pytest.param(
+            "sh-okada",
+            "BadPassword123",
+            400,
+            {"detail": "Incorrect username or password."},
+            id="パスワードが間違っている場合、ログインできないこと",
+        ),
+    ],
+)
+def test_JSONレスポンス(
+    username: str, password: str, status_code: int, json_response: dict | None
+):
     user = db_models.User(
         id=uuid.UUID("5e3868cd-3ec0-4f86-9a94-84363c64da29"),
         name="sh-okada",
@@ -24,70 +55,10 @@ def before_each():
     session.add(user)
     session.commit()
 
-
-@pytest.mark.parametrize(
-    "username, password, status_code",
-    [
-        pytest.param(
-            "sh-okada",
-            "Password123",
-            200,
-            id="ユーザー名とパスワードが正しい場合",
-        ),
-        pytest.param(
-            "bad-user",
-            "Password123",
-            400,
-            id="ユーザー名が間違っている場合",
-        ),
-        pytest.param(
-            "sh-okada",
-            "BadPassword123",
-            400,
-            id="パスワードが間違っている場合",
-        ),
-    ],
-)
-def test_ステータスコード(username: str, password: str, status_code: int):
     with freeze_time(datetime(2025, 7, 23, 0, 0, 0)):
         response = client.post(
             "/api/auth/login", data={"username": username, "password": password}
         )
 
     assert response.status_code == status_code
-
-
-@pytest.mark.parametrize(
-    "username, password, json_response",
-    [
-        pytest.param(
-            "sh-okada",
-            "Password123",
-            {
-                "id": "5e3868cd-3ec0-4f86-9a94-84363c64da29",
-                "username": "sh-okada",
-                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTM4NjhjZC0zZWMwLTRmODYtOWE5NC04NDM2M2M2NGRhMjkiLCJleHAiOjE3NTMyMzA2MDB9.cTPZJnsl44nXmY9dfDS7mOc1s11mjCenmdy14FH_wzg",
-            },
-            id="ユーザー名とパスワードが正しい場合",
-        ),
-        pytest.param(
-            "bad-user",
-            "Password123",
-            {"detail": "Incorrect username or password."},
-            id="ユーザー名が間違っている場合",
-        ),
-        pytest.param(
-            "sh-okada",
-            "BadPassword123",
-            {"detail": "Incorrect username or password."},
-            id="パスワードが間違っている場合",
-        ),
-    ],
-)
-def test_JSONレスポンス(username: str, password: str, json_response: dict | None):
-    with freeze_time(datetime(2025, 7, 23, 0, 0, 0)):
-        response = client.post(
-            "/api/auth/login", data={"username": username, "password": password}
-        )
-
     assert response.json() == json_response
