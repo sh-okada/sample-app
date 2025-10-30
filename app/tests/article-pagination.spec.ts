834a8f2e58826c8d0f74ec6c16ae09b8a6466085
sh-okada <sh-okada@ymail.ne.jp>
2025-10-30 20:55:56 +0900

    データ修正

--- app/tests/article-pagination.spec.ts
+++ app/tests/article-pagination.spec.ts
@@ -12,7 +12,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
         values: [
           {
             id: "51a679c2-2062-4e6c-93b3-af1c681bb281",
-            title: "1",
+            title: "title1",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -22,7 +22,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "69c042b2-85c8-418a-899a-dbcb62cad338",
-            title: "2",
+            title: "title2",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -32,7 +32,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "abd32f0d-2a34-4ece-bc79-fadb37a51353",
-            title: "3",
+            title: "title3",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -42,7 +42,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "4ae7a5a9-27da-4f36-a130-d6e9319cc052",
-            title: "4",
+            title: "title4",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -52,7 +52,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "8e217aff-19cd-46da-9104-64e26fef303f",
-            title: "5",
+            title: "title5",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -77,7 +77,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
         values: [
           {
             id: "a4404bbe-c8b8-40c3-98d4-8f2754251464",
-            title: "6",
+            title: "title6",
             text: "# Hello World",
             published_at: "2025-07-23T23:00:00Z",
             user: {
@@ -87,7 +87,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "a11fd7c1-6cee-4ec9-aba5-090cbbd37c22",
-            title: "7",
+            title: "title7",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -97,7 +97,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "44eee787-4081-4500-a04d-22c0b670c296",
-            title: "8",
+            title: "title8",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -107,7 +107,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "f48432e7-dc41-4f1f-b7b7-2038e9ebbb23",
-            title: "9",
+            title: "title9",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -117,7 +117,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "6a416cd6-a24c-4057-b483-08d2547f23a7",
-            title: "10",
+            title: "title10",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -142,7 +142,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
         values: [
           {
             id: "1fd47c4f-c936-495b-85e1-c7e0b1618dcc",
-            title: "11",
+            title: "title11",
             text: "# Hello World",
             published_at: "2025-07-23T23:00:00Z",
             user: {
@@ -152,7 +152,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "ae16789a-cd11-404b-982f-69a3e7617bed",
-            title: "12",
+            title: "title12",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -162,7 +162,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "bec88aa2-5690-407f-bc40-8e8d7da02883",
-            title: "13",
+            title: "title13",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -172,7 +172,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "8f040903-df95-4e53-8cde-d23c72785457",
-            title: "14",
+            title: "title14",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
@@ -182,7 +182,7 @@ test.beforeEach(async ({ mockServerRequest }) => {
           },
           {
             id: "7c514b79-ac95-4368-a365-ed8043eb236c",
-            title: "15",
+            title: "title15",
             text: "# Hello World",
             published_at: "2025-07-23T00:00:00",
             user: {
